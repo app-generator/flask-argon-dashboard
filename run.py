53010ece259051f2644f8e48c26134f4b2a428cc
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-03 17:34:19 +0200

    Update run.py

--- run.py
+++ run.py
@@ -32,11 +32,9 @@ if not DEBUG:
     Minify(app=app, html=True, js=False, cssless=False)
     
 if DEBUG:
-    app.logger.info('DEBUG            = ' + str(DEBUG)             )
-    app.logger.info('FLASK_ENV        = ' + os.getenv('FLASK_ENV') )
+    app.logger.info('DEBUG            = ' + str(DEBUG) )
     app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
     app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
-    app.logger.info('ASSETS_ROOT      = ' + app_config.ASSETS_ROOT )
 
 if __name__ == "__main__":
     app.run()
