53f84210a190743814c4aa1af1cb8dc9d099021e
App Generator <51070104+app-generator@users.noreply.github.com>
2023-04-02 08:24:19 +0300

    Added Gulp Tooling

---
+++ apps/static/assets/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var path = require('path');
+var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var open = require('gulp-open');
+
+var Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './css/',
+  SCSS_TOOLKIT_SOURCES: './scss/argon-dashboard.scss',
+  SCSS: './scss/**/**'
+};
+
+gulp.task('scss', function() {
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
+});
+
+gulp.task('open', function() {
+  gulp.src('pages/dashboard.html')
+    .pipe(open());
+});
+
+gulp.task('open-app', gulp.parallel('open', 'watch'));
